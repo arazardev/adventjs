db4f1af3084d0ae5d13f2b837a5225a37fd67f9d
Esteban Sonaglioni <sonaglioni36467@estudiantes.untref.edu.ar>
2023-12-04 20:45:00 -0300

    challenge 04 solved

---
+++ 2023/4/index.js
@@ -0,0 +1,25 @@
+function decode(message) {
+    let from = parseInt(message.indexOf("("))
+    let to = parseInt(message.indexOf(")"))
+    if (from === -1 || to === -1){
+        return message
+    }
+    for (let i = 0;i<message.length; i++) {
+        if (message[i] === "("){
+            from = i+1
+        } else  if (message[i] === ")"){
+                to = i
+                break
+        }
+    }
+    let inBrackets = message.slice(from, to)
+    let inBracketsArray = []
+    for (const letter of inBrackets){
+        inBracketsArray.push(letter)
+    }
+    let inBracketsInversed = inBracketsArray.reverse().join("")
+    let result = message.replace(`(${inBrackets})`,inBracketsInversed)
+    return  decode(result)
+  }
+
+console.log(decode('sa(u(cla)atn)s'))
